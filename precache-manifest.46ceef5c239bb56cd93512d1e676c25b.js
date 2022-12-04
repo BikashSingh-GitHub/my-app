f44fda1c33948ffa091c09f2f54157a349fdf556
Bikash Singh <blackhatsys@gmail.com>
2022-12-04 13:16:14 +0530

    Updates

---
+++ precache-manifest.46ceef5c239bb56cd93512d1e676c25b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "98dc01bd73e6b198aaac",
+    "url": "/my-app/static/css/main.4778ce26.chunk.css"
+  },
+  {
+    "revision": "98dc01bd73e6b198aaac",
+    "url": "/my-app/static/js/main.98dc01bd.chunk.js"
+  },
+  {
+    "revision": "bfa36157bcc75fb8ea07",
+    "url": "/my-app/static/js/1.bfa36157.chunk.js"
+  },
+  {
+    "revision": "7587d2c9267632307ee0",
+    "url": "/my-app/static/js/2.7587d2c9.chunk.js"
+  },
+  {
+    "revision": "7d8bd7cc46eb52dee2da",
+    "url": "/my-app/static/js/runtime~main.7d8bd7cc.js"
+  },
+  {
+    "revision": "1c740eb04cc65f160a227580e229a957",
+    "url": "/my-app/index.html"
+  }
+];
